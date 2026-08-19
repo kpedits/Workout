f0520dee4ab6718836da2ef4a63241a3b8322ce1
kpedits <kpedits1234@gmail.com>
2026-08-19 19:12:32 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const CACHE="workout-tracker-v7-final";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "workout-tracker-v8-layout-fix";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((k) => Promise.all(k.filter((x) => x !== CACHE).map((x) => caches.delete(x)))));
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(fetch(e.request).catch(() => caches.match("./index.html")));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
