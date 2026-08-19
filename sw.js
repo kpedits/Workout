2abda981e8dd7ae489abd521bf5aba1d25eec638
kpedits <kpedits1234@gmail.com>
2026-08-19 19:23:04 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workout-tracker-v8-layout-fix";
+const CACHE = "workout-tracker-v9-exercise-filter";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
